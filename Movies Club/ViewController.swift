0a8d2f15126109b3396f688710ad3bbd672e4ecb
ahmedrefat21 <ahmedref21@gmail.com>
2024-08-10 19:18:13 +0300

    Initial commit

---
+++ Movies Club/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies Club
+//
+//  Created by Ahmed Refat on 10/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+//Initial commit
